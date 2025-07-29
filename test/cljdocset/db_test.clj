92413d5dfc29f1e9d2f130230c60ae9a5adc4ba5
Casey Link <unnamedrambler@gmail.com>
2025-07-29 10:45:34 +0200

    Update license headers

--- test/cljdocset/db_test.clj
+++ test/cljdocset/db_test.clj
@@ -1,3 +1,5 @@
+;; Copyright © 2025 Casey Link <casey@outskirtslabs.com>
+;; SPDX-License-Identifier: EUPL-1.2
 (ns cljdocset.db-test
   "Tests for SQLite database operations"
   (:require
--- test/cljdocset/download_test.clj
+++ test/cljdocset/download_test.clj
@@ -1,3 +1,5 @@
+;; Copyright © 2025 Casey Link <casey@outskirtslabs.com>
+;; SPDX-License-Identifier: EUPL-1.2
 (ns cljdocset.download-test
   (:require
    [babashka.fs :as fs]
